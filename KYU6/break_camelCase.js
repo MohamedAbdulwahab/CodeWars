1472a91aa906b96583598a6cfb6f1024528b2397
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2022-07-13 17:33:54 -0700

    added solution

--- KYU6/break_camelCase.js
+++ KYU6/break_camelCase.js
@@ -1,6 +1,6 @@
 /* 
 DESCRIPTION:
-Complete the solution so that the function will break up camel casing, using a space between words.
+Complete the solution so that the function will break up camel casing, using a space between words
 
 Example
 "camelCasing"  =>  "camel Casing"

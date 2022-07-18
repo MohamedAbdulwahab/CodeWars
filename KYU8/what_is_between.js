5afbad7fe68f83965adf889b85369d4a23741710
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2022-07-18 01:11:02 -0700

    added solution

---
+++ KYU8/what_is_between.js
@@ -0,0 +1,20 @@
+/*
+Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+
+For example:
+
+a = 1
+b = 4
+return: [1, 2, 3, 4] 
+*/
+
+// solution
+function between(a, b) {
+	const arr = [];
+	
+	for(let i = a; i <= b; i++) {
+	  arr.push(i);
+	}
+	
+	return arr;
+}

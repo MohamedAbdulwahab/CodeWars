16006cd4851db24069921133631a799ef31eb8a5
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2022-07-18 01:07:43 -0700

    added solution

---
+++ KYU8/remove_exclamation_marks.js
@@ -0,0 +1,7 @@
+/*
+DESCRIPTION:
+Write function RemoveExclamationMarks which removes all exclamation marks from a given string. 
+*/
+
+// solution
+const removeExclamationMarks = (s) => s.replace(/!/g, '');

a4bb206a826d525c11782fe237e4cbf5f03ec06a
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2022-08-04 12:12:37 -0700

    added solution

---
+++ KYU8/hex_to_decimal.js
@@ -0,0 +1,10 @@
+/*
+DESCRIPTION:
+
+Complete the function which converts hex number (given as a string) to a decimal number.
+*/
+
+// solution
+function hexToDec(hexString) {
+	return parseInt(hexString, 16);
+}

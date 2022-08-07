59588ce32cc79319ce1f394cf049319840251560
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2022-08-07 16:49:57 -0700

    added solution

---
+++ KYU6/frog_jumping.js
@@ -0,0 +1,40 @@
+/*
+DESCRIPTION:
+
+Help the frog to find a way to freedom
+You have an array of integers and have a frog at the first position
+
+[Frog, int, int, int, ..., int]
+
+The integer itself may tell you the length and the direction of the jump
+
+For instance:
+ 2 = jump two indices to the right
+-3 = jump three indices to the left
+ 0 = stay at the same position
+Your objective is to find how many jumps are needed to jump out of the array.
+
+Return -1 if Frog can't jump out of the array
+
+Example:
+array = [1, 2, 1, 5]; 
+jumps = 3  (1 -> 2 -> 5 -> <jump out>)
+All tests for this Kata are randomly generated.
+*/
+
+// solution
+function solution(a) {
+	let index = 0;
+	let jumps = 0;
+  
+	while(typeof a[index] !== 'undefined') {
+	  jumps++;
+	  index += a[index];
+  
+	  if(jumps > a.length) {
+		return -1;
+	  }
+	}
+  
+	return jumps;
+}

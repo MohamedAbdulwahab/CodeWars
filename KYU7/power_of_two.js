dd5b787cad92d24cc7f8bc7f80575c10406a081f
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2022-08-17 15:45:16 -0700

    added solution

---
+++ KYU7/power_of_two.js
@@ -0,0 +1,25 @@
+/*
+DESCRIPTION:
+Complete the function power_of_two/powerOfTwo (or equivalent, depending on your language) that determines if a given non-negative integer is a power of two. From the corresponding Wikipedia entry:
+
+a power of two is a number of the form 2n where n is an integer, i.e. the result of exponentiation with number two as the base and integer n as the exponent.
+
+You may assume the input is always valid.
+
+Examples
+isPowerOfTwo(1024) // -> true
+isPowerOfTwo(4096) // -> true
+isPowerOfTwo(333)  // -> false
+Beware of certain edge cases - for example, 1 is a power of 2 since 2^0 = 1 and 0 is not a power of 2.
+*/
+
+// solution
+function isPowerOfTwo(n) {  
+	// divide n by 2 as long as n is >= 2.
+	while(n >= 2) {
+	  n /= 2;
+	}
+	
+	// now n should either be 1 (power of 2) or a number < 1 (not a power of 2).
+	return n === 1;
+}

b2f641721ab1c7b10c4d7c2f41cf6441ef36e71c
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2022-08-18 14:11:33 -0700

    added solution

---
+++ KYU8/are_arrow_functions_odd.js
@@ -0,0 +1,10 @@
+/*
+DESCRIPTION:
+Time to test your basic knowledge in functions! Return the odds from a list:
+
+[1, 2, 3, 4, 5]  -->  [1, 3, 5]
+[2, 4, 6]        -->  []
+*/
+
+// solution
+const odds = values => values.filter(e => e % 2 !== 0);

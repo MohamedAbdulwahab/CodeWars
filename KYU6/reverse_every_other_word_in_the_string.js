441dbca6a6b7601f3cecffec3286983b101e6973
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2022-10-03 14:01:05 -0700

    added solution

--- KYU6/reverse_every_other_word_in_the_string.js
+++ KYU6/reverse_every_other_word_in_the_string.js
@@ -3,7 +3,7 @@ DESCRIPTION:
 Reverse every other word in a given string, then return the string. Throw away any leading or trailing whitespace, while ensuring there is exactly one space between each word. Punctuation marks should be treated as if they are a part of the word in this kata.
 */
 
-// solution
+// solution 1:
 function reverse(str) {
 	const strArr = str.split(' ');
   
@@ -12,4 +12,15 @@ function reverse(str) {
 	}
   
 	return (str == false) ? '' : strArr.join(' ');
+}
+
+// solution 2:
+function reverse(str) {
+	const strArr = str.split(' ');
+  
+	for(let i = 1; i < strArr.length; i += 2) {
+	  strArr[i] = strArr[i].split('').reverse().join('');
+	}
+  
+	return strArr.join(' ').trim(' ');
 }

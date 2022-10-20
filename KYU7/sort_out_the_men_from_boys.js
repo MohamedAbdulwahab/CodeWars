8b3e07f9797307f1fb3fd9ca32f404540abd76a2
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2022-10-19 20:46:08 -0700

    added solution

--- KYU7/sort_out_the_men_from_boys.js
+++ KYU7/sort_out_the_men_from_boys.js
@@ -18,17 +18,17 @@ Notes:
 Examples:
 menFromBoys ({7, 3 , 14 , 17}) ==> return ({14, 17, 7, 3}) 
 Explanation:
-Since , { 14 } is the even number here , So it came first , then the odds in descending order {17 , 7 , 3} .
+Since, { 14 } is the even number here , So it came first , then the odds in descending order {17 , 7 , 3} .
 
 menFromBoys ({-94, -99 , -100 , -99 , -96 , -99 }) ==> return ({-100 , -96 , -94 , -99})
 Explanation:
-Since , { -100, -96 , -94 } is the even numbers here , So it came first in *ascending order *, then the odds in descending order { -99 }
-Since , (Duplications are not included when separating) , then you can see only one (-99) was appeared in the final array/list .
+Since, { -100, -96 , -94 } is the even numbers here , So it came first in *ascending order *, then the odds in descending order { -99 }
+Since, (Duplications are not included when separating) , then you can see only one (-99) was appeared in the final array/list .
 
 menFromBoys ({49 , 818 , -282 , 900 , 928 , 281 , -282 , -1 }) ==> return ({-282 , 818 , 900 , 928 , 281 , 49 , -1})
 Explanation:
-Since , {-282 , 818 , 900 , 928 } is the even numbers here , So it came first in ascending order , then the odds in descending order { 281 , 49 , -1 }
-Since , (Duplications are not included when separating) , then you can see only one (-282) was appeared in the final array/list .
+Since, {-282 , 818 , 900 , 928 } is the even numbers here , So it came first in ascending order , then the odds in descending order { 281 , 49 , -1 }
+Since, (Duplications are not included when separating) , then you can see only one (-282) was appeared in the final array/list .
 */
 
 // solution

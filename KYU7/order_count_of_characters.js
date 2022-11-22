0ba85d15bf02165f824690d7d129babbfbe06140
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2022-11-21 17:25:14 -0800

    added solution

--- KYU7/order_count_of_characters.js
+++ KYU7/order_count_of_characters.js
@@ -8,8 +8,29 @@ Example:
 orderedCount("abracadabra") == [['a', 5], ['b', 2], ['r', 2], ['c', 1], ['d', 1]]
 */
 
-// solution
+// solution 1:
 const orderedCount = (text) => {
 	const arr = [...new Set([...text])]
 	return arr.map(el => [el, [...text].filter(e => e === el).length]);
+}
+
+// solution 2: 
+const orderedCount = function (text) {
+	const counter = {};
+	const list = [];
+	
+	for(let i = 0; i < text.length; i++) {
+	  if(!counter.hasOwnProperty([text[i]])) {
+		counter[text[i]] = 1;
+	  } else {
+		counter[text[i]]++;
+	  }
+	}
+  
+	for(const property in counter) {
+	  list.push([property, counter[property]]);
+	}
+	
+  
+	return list;
 }

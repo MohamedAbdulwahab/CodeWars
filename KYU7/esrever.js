29b3278ae492ab59ce89c7621cf5d585023b0b5a
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2022-11-24 12:48:59 -0800

    added solution

---
+++ KYU7/esrever.js
@@ -0,0 +1,17 @@
+/*
+DESCRIPTION:
+Write a function reverse which reverses a list (or in clojure's case, any list-like data structure)
+
+(the dedicated builtin(s) functionalities are deactivated)
+*/
+
+// solution
+reverse = function(array) {
+	const reversed = [];
+  
+	for(let i = array.length - 1; i >= 0; i--) {
+	  reversed.push(array[i]);
+	}
+  
+	return reversed;
+}

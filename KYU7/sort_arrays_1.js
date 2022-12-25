ce110c0a5fec4705234e133db1d80c3b04ca64be
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2022-12-25 14:04:35 -0800

    added solution

---
+++ KYU7/sort_arrays_1.js
@@ -0,0 +1,7 @@
+/*
+DESCRIPTION:
+Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+*/
+
+// solution
+const sortme = (names) => names.sort();

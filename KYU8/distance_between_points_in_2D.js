2659fb5b44e2b87b1c57872e41024de5c3e4cc78
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2022-12-27 13:33:49 -0800

    added solution

---
+++ KYU8/distance_between_points_in_2D.js
@@ -0,0 +1,13 @@
+/*
+DESCRIPTION:
+This series of katas will introduce you to basics of doing geometry with computers.
+
+Point objects have x and y attributes (X and Y in C#) attributes.
+
+Write a function calculating distance between Point a and Point b.
+
+Tests round answers to 6 decimal places.
+*/
+
+// solution
+const distanceBetweenPoints = (a, b) => Math.hypot((a.x - b.x), (a.y - b.y));

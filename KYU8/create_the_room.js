c304b2936dd056204ee370591366c22dd4ff5fb6
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2023-01-05 11:33:48 -0800

    added solution

---
+++ KYU8/create_the_room.js
@@ -0,0 +1,23 @@
+/*
+DESCRIPTION:
+You are creating an "Escape the room" game. The first step is to create a hash table called rooms that contains all of the rooms of the game. There should be at least 3 rooms inside it, each being a hash table with at least three properties (e.g. name, description, completed).
+*/
+
+// solution
+const rooms = {
+  room1: {
+    name: undefined,
+    description: undefined,
+    completed: undefined
+  },
+  room2: {
+    name: undefined,
+    description: undefined,
+    completed: undefined
+  },
+  room3: {
+    name: undefined,
+    description: undefined,
+    completed: undefined
+  }
+};
